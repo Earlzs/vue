c0b05e53f9b8345342456a9d7449ebd904f2a14a
zs <earlzs@163.com>
2017-08-21 00:14:32 +0800

    '1'

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -2,12 +2,15 @@
 <transition name='songList'>
     <div class='songList' v-show="showSonglist">
         <div class='song-header'>
-            <div class='title'>
-                <div> <i class="fa fa-arrow-left" aria-hidden="true"></i><span> Music list</span></div>
-                <i class="fa fa-search" aria-hidden="true"></i>
+            <div>
+                <i class="fa fa-arrow-left" aria-hidden="true"></i><span> Music list</span>
             </div>
+            <i class="fa fa-search" aria-hidden="true"></i>
+        </div>
 
-            <div class='musicDetail'>
+
+        <div class='musicDetail'>
+            <div class='musicDetail-top clearfix'>
                 <img src="http://oiq8j9er1.bkt.clouddn.com/music_owl_city.jpg" alt="">
                 <div class='musicDetail-r'>
                     <div class='detailname'>在欧洲田园般的乡村上骑单车</div>
@@ -16,25 +19,47 @@
                         <span>user.name </span> >
                     </div>
                 </div>
+
+                <div class='operation'>
+                    <div>
+                        <i class="fa fa-life-ring" aria-hidden="true"></i>
+                        <br> 收藏
+                    </div>
+                    <div>
+                        <i class="fa fa-american-sign-language-interpreting" aria-hidden="true"></i> <br> 评论
+                    </div>
+                    <div>
+                        <i class="fa fa-coffee" aria-hidden="true"></i> <br> 分享
+                    </div>
+                    <div>
+                        <i class="fa fa-sign-language" aria-hidden="true"></i> <br> 下载
+                    </div>
+                </div>
             </div>
 
-            <div class='operation'>
-                <div>
-                     <i class="fa fa-life-ring" aria-hidden="true"></i>
-                     <br>
-                    收藏
+
+            <div class='musicDetail-bottom'>
+                <div class='bootom-warpper'>
+                    <div>
+                        <i class="fa fa-music" aria-hidden="true"></i> 播放全部(共19首)
+                    </div>
+                    <div>
+                        <i class="fa fa-music" aria-hidden="true"></i> 多选
+                    </div>
                 </div>
-                 <div>
-                    <i class="fa fa-american-sign-language-interpreting" aria-hidden="true"></i> <br>
-                    评论
-                </div>
-                 <div>
-                    <i class="fa fa-coffee" aria-hidden="true"></i>  <br>
-                    分享
-                </div>
-                 <div>
-                    <i class="fa fa-sign-language" aria-hidden="true"></i> <br>
-                    下载
+
+                <div class='song-list'>
+
+                    <div class='warpper'>
+                        <div class='song-index'>
+                            1
+                        </div>
+                        <div class='song-info'>
+                                <span class='info-name'>2121</span>
+                                <span class='info-author'>212121</span>
+                        </div>
+                         <i class="fa fa-music" aria-hidden="true"></i> 
+                    </div>
                 </div>
             </div>
 
